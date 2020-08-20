ef946f30453dfce20bdf7ee336154b4093c3114d
aniljaiswal <aniljaiswal@coviam.com>
2020-08-20 21:30:21 +0530

    ViewModel, datafactory  and all code are completed.

--- app/src/main/java/com/example/pagingsample/MainActivity.java
+++ app/src/main/java/com/example/pagingsample/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.pagingsample;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.paging.PagedList;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private RecyclerView recyclerView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        ItemViewModel itemViewModel = ViewModelProviders.of(this).get(ItemViewModel.class);
+        final ItemAdapter adapter = new ItemAdapter(this);
+
+        itemViewModel.itemPagedList.observe(this, new Observer<PagedList<Item>>() {
+            @Override
+            public void onChanged(@Nullable PagedList<Item> items) {
+                adapter.submitList(items);
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
     }
 }
